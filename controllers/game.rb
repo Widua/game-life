90dad5d9952758857056f3e99920d4c6cfdb413c
Widua <niyaz-yakupov13@yandex.ru>
2016-03-28 23:13:42 +0300

    add incoming file, write private function for data assignment, correct expect data

--- controllers/game.rb
+++ controllers/game.rb
@@ -2,6 +2,15 @@ class Game
   attr_accessor :data
 
   def initialize
-    @data = ''
+    @data = first_data
+  end
+
+  private
+
+  def first_data
+    f = File.open('initial_data.txt')
+    initial_data = f.first.chomp.split(', ')
+    f.close
+    initial_data
   end
 end
--- spec/controllers/game_spec.rb
+++ spec/controllers/game_spec.rb
@@ -4,7 +4,7 @@ describe Game do
   describe 'build' do
     it 'add to game data' do
       game = Game.new
-      expect(game.data).to eq '10, 10, [0,0], [2,0], [1,1], [2,1], [1,2]'
+      expect(game.data).to eq ["10", "10", "[0,0]", "[2,0]", "[1,1]", "[2,1]", "[1,2]"]
     end
   end
 end
