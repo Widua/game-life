acee5fe830b05325ddd4bfc934891c8a7c344d58
Widua <niyaz-yakupov13@yandex.ru>
2016-03-28 23:20:33 +0300

    create universe for game test

--- spec/controllers/game_spec.rb
+++ spec/controllers/game_spec.rb
@@ -2,9 +2,15 @@ require './controllers/game'
 
 describe Game do
   describe 'build' do
+    let(:game) { Game.new }
+    let(:universe) { Universe.new(10, 10) }
+
     it 'add to game data' do
-      game = Game.new
       expect(game.data).to eq ["10", "10", "[0,0]", "[2,0]", "[1,1]", "[2,1]", "[1,2]"]
+    end
+
+    it 'add to game universe' do
+      expect(game.universe).to eq universe
     end
   end
 end
