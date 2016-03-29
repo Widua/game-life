a69698bc95077916e5eb275e207319d4bb310095
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 21:39:57 +0300

    test for create information model and set rows for it

---
+++ spec/models/information_spec.rb
@@ -0,0 +1,9 @@
+describe Information do
+  describe 'initialize' do
+    let(:information) { Information.new }
+
+    it 'set rows from file' do
+      expect(information.rows).to eq 10
+    end
+  end
+end
