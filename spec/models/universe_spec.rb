d79527c79c5fde47e3743cf70aa385595be94595
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:46:15 +0300

    need fill universe table with cells

--- spec/models/universe_spec.rb
+++ spec/models/universe_spec.rb
@@ -7,5 +7,9 @@ describe Universe do
     it 'draw empty table' do
       expect(universe.table).to eq [[0,0],[0,0]]
     end
+
+    it 'create dead cells' do
+      expect(universe.cells.count).to eq 4
+    end
   end
 end
