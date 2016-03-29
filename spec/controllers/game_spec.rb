347080c9b241df349ae4acf5e30bb6b52c999cad
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:01:50 +0300

    delete data from game test

--- spec/controllers/game_spec.rb
+++ spec/controllers/game_spec.rb
@@ -15,5 +15,9 @@ describe Game do
     it 'add to game information' do
       expect(game.information).to be_a(Information)
     end
+
+    it 'does not response to data' do
+      expect{ game.data }.to raise_error
+    end
   end
 end
