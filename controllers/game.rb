db79414aeb9f619e8ce2d8163656a4f952df7da4
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:07:21 +0300

    delete data from game, fix tests, and delete unnecessary methods

--- controllers/game.rb
+++ controllers/game.rb
@@ -2,28 +2,10 @@ require './models/universe'
 require './models/information'
 
 class Game
-  attr_accessor :information, :data, :universe
+  attr_accessor :information, :universe
 
   def initialize
     @information = Information.new
-    @data = first_data
-    @universe = Universe.new(rows, columns)
-  end
-
-  private
-
-  def first_data
-    f = File.open('initial_data.txt')
-    initial_data = f.first.chomp.split(', ')
-    f.close
-    initial_data
-  end
-
-  def rows
-    data[0].to_i
-  end
-
-  def columns
-    data[1].to_i
+    @universe = Universe.new(information.rows, information.columns)
   end
 end
--- spec/controllers/game_spec.rb
+++ spec/controllers/game_spec.rb
@@ -4,10 +4,6 @@ describe Game do
   describe 'build' do
     let(:game) { Game.new }
 
-    it 'add to game data' do
-      expect(game.data).to eq ["10", "10", "[0,0]", "[2,0]", "[1,1]", "[2,1]", "[1,2]"]
-    end
-
     it 'add to game universe' do
       expect(game.universe).to be_a(Universe)
     end
@@ -17,7 +13,7 @@ describe Game do
     end
 
     it 'does not response to data' do
-      expect{ game.data }.to raise_error
+      expect{ game.data }.to raise_error(NoMethodError)
     end
   end
 end
