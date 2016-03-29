220e781baa086dad133d683a321039f3d6d7b1ee
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:59:56 +0300

    add coordinates attribute to information

--- models/information.rb
+++ models/information.rb
@@ -1,12 +1,20 @@
 class Information
-  attr_accessor :rows, :columns
+  attr_accessor :rows, :columns, :coordinates
 
   def initialize
     @rows = set_rows
     @columns = set_columns
+    @coordinates = set_coordinates
   end
 
   private
+
+  def file_data
+    f = File.open('initial_data.txt')
+    initial_data = f.first.chomp.split(', ')
+    f.close
+    initial_data
+  end
 
   def set_rows
     file_data[0].to_i
@@ -16,10 +24,13 @@ class Information
     file_data[1].to_i
   end
 
-  def file_data
-    f = File.open('initial_data.txt')
-    initial_data = f.first.chomp.split(', ')
-    f.close
-    initial_data
+  def set_coordinates
+    cell_coordinates = []
+
+    file_data[2..-1].each do |value|
+      cell_coordinates << value.delete('[]').split(',').map(&:to_i)
+    end
+
+    cell_coordinates
   end
 end
