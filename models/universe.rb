7a031ebeed92f348df5eea56349b2ce25c0c38de
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:26:49 +0300

    fill table when universe intialize

--- models/universe.rb
+++ models/universe.rb
@@ -1,8 +1,15 @@
 class Universe
-  attr_accessor :rows, :columns
+  attr_accessor :rows, :columns, :table
 
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
+    @table = fill_table
+  end
+
+  private
+
+  def fill_table
+    Array.new(rows) { Array.new(columns, 0) }
   end
 end
--- spec/models/universe_spec.rb
+++ spec/models/universe_spec.rb
@@ -4,7 +4,7 @@ describe Universe do
   let(:universe) { Universe.new(2, 2) }
 
   describe 'initialize' do
-    it 'draw universe table' do
+    it 'draw empty table' do
       expect(universe.table).to eq [[0,0],[0,0]]
     end
   end
