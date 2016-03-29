315bb333bfe2bfc8ba07accd9d70f1d7fc266139
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 22:51:40 +0300

    add cells attribute to universe and create new cells

--- models/universe.rb
+++ models/universe.rb
@@ -1,15 +1,30 @@
+require './models/cell'
+
 class Universe
-  attr_accessor :rows, :columns, :table
+  attr_accessor :rows, :columns, :table, :cells
 
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
     @table = fill_table
+    @cells = create_cells
   end
 
   private
 
   def fill_table
     Array.new(rows) { Array.new(columns, 0) }
+  end
+
+  def create_cells
+    cells = []
+
+    0.upto(rows - 1) do |i|
+      0.upto(columns - 1) do |y|
+        cells << Cell.new(i ,y)
+      end
+    end
+
+    cells
   end
 end
