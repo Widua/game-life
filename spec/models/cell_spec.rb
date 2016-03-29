5df58def0bc4a76d6402f49313d64ed27cbe0712
Widua <niyaz-yakupov13@yandex.ru>
2016-03-29 23:32:16 +0300

    create tests for cell toggle method

--- spec/models/cell_spec.rb
+++ spec/models/cell_spec.rb
@@ -1,8 +1,9 @@
 require './models/cell'
 
 describe Cell do
+  let(:cell) { Cell.new(1,3) }
+
   describe 'initialize' do
-    let(:cell) { Cell.new(1,3) }
 
     it 'have x attribute' do
       expect(cell.x).to eq 1
@@ -13,6 +14,17 @@ describe Cell do
     end
 
     it 'have living attribute' do
+      expect(cell.living).to eq false
+    end
+  end
+
+  describe 'toggle' do
+    it 'change living to true' do
+      expect(cell.toggle!).to eq true
+    end
+
+    it 'change living to false' do
+      cell.living = true
       expect(cell.living).to eq false
     end
   end
