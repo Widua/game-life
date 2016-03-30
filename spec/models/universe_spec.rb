3adfd8e0f250e850adda23d1084bcef8994a33a6
Widua <niyaz-yakupov13@yandex.ru>
2016-03-30 19:28:58 +0300

    refactor universe tests

--- spec/models/universe_spec.rb
+++ spec/models/universe_spec.rb
@@ -1,41 +1,36 @@
 require './models/universe'
 
 describe Universe do
+  let(:universe) { Universe.new(5,5) }
+  let(:information) { Information.new }
+
+  before do
+    universe.revive_cells(information.coordinates)
+  end
+
   describe 'initialize' do
-    let(:universe) { Universe.new(2, 2) }
+    let(:universe_two) { Universe.new(2,2) }
 
     it 'draw empty table' do
-      expect(universe.table).to eq [[0,0],[0,0]]
+      expect(universe_two.table).to eq [[0,0],[0,0]]
     end
 
     it 'create dead cells' do
-      expect(universe.cells.count).to eq 4
+      expect(universe_two.cells.count).to eq 4
     end
   end
 
   describe 'revive_cells' do
-    let(:universe) { Universe.new(5,5) }
-    let(:information) { Information.new }
-
     it 'update cells living attribute' do
-      universe.revive_cells(information.coordinates)
       expect(universe.cells.first.living).to eq true
     end
 
     it 'mark cells in table' do
-      universe.revive_cells(information.coordinates)
       expect(universe.table[0][0]).to eq 1
     end
   end
 
   describe 'display' do
-    let(:universe) { Universe.new(5,5) }
-    let(:information) { Information.new }
-
-    before do
-      universe.revive_cells(information.coordinates)
-    end
-
     it 'mark living cells as *' do
       expect(universe.display.count('*')).to eq 5
     end
@@ -46,11 +41,7 @@ describe Universe do
   end
 
   describe 'living_neighbors_count' do
-    let(:universe) { Universe.new(5,5) }
-    let(:information) { Information.new }
-
     it 'return number of living cell near' do
-      universe.revive_cells(information.coordinates)
       expect(universe.living_neighbors_count(0, 0)).to eq 1
       expect(universe.living_neighbors_count(0, 1)).to eq 3
       expect(universe.living_neighbors_count(1, 2)).to eq 2
