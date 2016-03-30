abe0f858166416db8d362b89475e11c54f2950fa
Widua <niyaz-yakupov13@yandex.ru>
2016-03-30 18:56:46 +0300

    need create method that return count of living cells around

--- spec/models/universe_spec.rb
+++ spec/models/universe_spec.rb
@@ -44,4 +44,16 @@ describe Universe do
       expect(universe.display.count(' ')).to eq 20
     end
   end
+
+  describe 'living_neighbors_count' do
+    let(:universe) { Universe.new(5,5) }
+    let(:information) { Information.new }
+
+    it 'return number of living cell near' do
+      universe.revive_cells(information.coordinates)
+      expect(universe.living_neighbors_count(0, 0)).to eq 1
+      expect(universe.living_neighbors_count(0, 1)).to eq 3
+      expect(universe.living_neighbors_count(1, 2)).to eq 2
+    end
+  end
 end
